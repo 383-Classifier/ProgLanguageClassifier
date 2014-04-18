23fcc34aeaea2eb3da039c7a491fd6c2acbf90cb
Jeremy Hummel <jmhummel@gmail.com>
2014-04-17 22:26:13 -0400

    Made minor updates to NBCBagger

--- src/NBCBagger.java
+++ src/NBCBagger.java
@@ -1,10 +1,26 @@
 import java.util.Scanner;
 import java.util.HashMap;
 import java.io.File;
+import java.io.IOException;
 
-abstract class NBCBagger{
+public class NBCBagger{
 
-	HashMap<String, Integer> makeBag(File file) throws IOExcetion{
+	private Classifier nbc;
+	
+	public NBCBagger() {
+		setClassifier(new Classifier());
+	}
+	
+	public Classifier getClassifier() {
+		return nbc;
+	}
+
+	public void setClassifier(Classifier nbc) {
+		this.nbc = nbc;
+	}
+
+	// Doesn't split properly yet
+	public HashMap<String, Integer> makeBag(File file) throws IOException{
 		Scanner sc = null;
 		String line = null;
 		sc = new Scanner(file);
@@ -14,10 +30,12 @@ abstract class NBCBagger{
 		*/
 		while(sc.hasNext()){
 			line = sc.next();
-			if(bag.containsKey(line))	bag.put(line,bag.get(line)+1);
-			else bag.put(line,1);
+			if(bag.containsKey(line))	
+				bag.put(line, bag.get(line)+1);
+			else 
+				bag.put(line, 1);
 		}
-		br.close();
+		sc.close();
 		return bag;
 	}
 

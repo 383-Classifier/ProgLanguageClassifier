7f8a77b90e88fb0941a7adb78a2d1b3ce6f79cd5
Jeremy Hummel <jmhummel@gmail.com>
2014-04-17 23:08:06 -0400

    Some file reader junk

--- src/NBCBagger.java
+++ src/NBCBagger.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
 import java.util.HashMap;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class NBCBagger{
@@ -19,23 +19,41 @@ public class NBCBagger{
 		this.nbc = nbc;
 	}
 
-	// Doesn't split properly yet
 	public HashMap<String, Integer> makeBag(File file) throws IOException{
-		Scanner sc = null;
-		String line = null;
-		sc = new Scanner(file);
+		FileReader reader = null;
 		HashMap<String,Integer> bag = new HashMap<String,Integer>();
-		/*
-		*	This doesn't split correctly yet; only on space delims probably.
-		*/
-		while(sc.hasNext()){
-			line = sc.next();
-			if(bag.containsKey(line))	
-				bag.put(line, bag.get(line)+1);
-			else 
-				bag.put(line, 1);
+	
+		try {
+			reader = new FileReader(file);
+			int c;
+			String token = "";
+			while ((c = reader.read()) != -1) {
+				if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
+					token += Character.toChars(c);
+				} else {
+					if(bag.containsKey(token)) {
+						bag.put(token, bag.get(token)+1);
+					} else {
+						bag.put(token, 1);
+					}
+					token = "";
+					token += Character.toChars(c);
+				}
+			}
+			if (token != "") {
+				if(bag.containsKey(token)) {
+					bag.put(token, bag.get(token)+1);
+				} else {
+					bag.put(token, 1);
+				}	
+			}
+			
+		} finally {
+			if (reader != null) {
+                reader.close();
+            }
 		}
-		sc.close();
+		
 		return bag;
 	}
 
